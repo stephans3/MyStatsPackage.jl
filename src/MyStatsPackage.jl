3a1f6d2be8b6d18fa16c7bdaae3f121e7d756eb7
Stephan Scholz <stephan.scholz@rwu.de>
2023-10-10 10:20:18 +0200

    Add printOwner function

--- src/MyStatsPackage.jl
+++ src/MyStatsPackage.jl
@@ -6,4 +6,10 @@ export StatResult
 export rse_sum, rse_mean, rse_std, rse_tstat
 include("statistic.jl")
 
+
+export printOwner
+function printOwner()
+    return "Stephan Scholz"
+end
+
 end # module MyStatsPackage
