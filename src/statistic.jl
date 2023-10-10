1123703082a3a6fb7d4bbbb4f87059b270f64397
Stephan Scholz <stephan.scholz@rwu.de>
2023-10-10 15:32:38 +0200

    Use JuliaFormatter

--- src/statistic.jl
+++ src/statistic.jl
@@ -1,38 +1,38 @@
-function rse_sum(a :: AbstractVector)
+function rse_sum(a::AbstractVector)
     # s = +(a...)
     @assert all(!isnan, a)
 
-    s=0
-    for (i,e) in enumerate(a)
+    s = 0
+    for (i, e) in enumerate(a)
         s += e
     end
 
     return s
 end
 
-function rse_mean(a :: AbstractVector)
+function rse_mean(a::AbstractVector)
     return rse_sum(a) / length(a)
 end
 
 
 
-function rse_std(a :: AbstractVector)
-    sqrt(rse_sum((a .- rse_mean(a)).^2) / (length(a)-1))
+function rse_std(a::AbstractVector)
+    sqrt(rse_sum((a .- rse_mean(a)) .^ 2) / (length(a) - 1))
 end
 
 
-function rse_tstat(a :: AbstractVector;σ=rse_std(a)) 
-    return rse_mean(a)/(σ/sqrt(length(a)))
+function rse_tstat(a::AbstractVector; σ = rse_std(a))
+    return rse_mean(a) / (σ / sqrt(length(a)))
 end
 
 struct StatResult
-    x; 
-    n; 
-    std;  
-    tvalue;
+    x::Any
+    n::Any
+    std::Any
+    tvalue::Any
 end
 
-function StatResult(a :: AbstractVector)
+function StatResult(a::AbstractVector)
     std = rse_std(a)
     tvalue = rse_tstat(a)
 
@@ -41,6 +41,6 @@ end
 
 
 
-function Base.length(sr :: StatResult)
+function Base.length(sr::StatResult)
     return sr.n
 end
